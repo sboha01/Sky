3eb5a422cfff517d76f030d25cd252ff6b6e5983
SaugatBohra <saugatdragon@gmail.com>
2013-09-15 16:17:29 -0400

    Created a rails application and statuses

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sky::Application.config.secret_token = 'f302b6dea66685b8a5c109ae8a342bcb10b7f1464e603a27b9d5b42e68f03904b56ade1c5c9a024dd4b99ddba246c8e5259a634d0fe7548864860aa56fcb55e0'
